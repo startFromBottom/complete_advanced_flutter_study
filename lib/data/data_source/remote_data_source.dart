95a52fea770a31b548e4ffebadb5d482a3500a1a
hyuem <uhh0701@gmail.com>
2022-04-11 00:24:08 +0900

    Lecture 53 - Data Layer - Remote Data Source

---
+++ lib/data/data_source/remote_data_source.dart
@@ -0,0 +1,25 @@
+import 'dart:developer';
+
+import 'package:complete_advanced_flutter/data/network/app_api.dart';
+import 'package:complete_advanced_flutter/data/request/request.dart';
+import 'package:complete_advanced_flutter/data/responses/responses.dart';
+
+abstract class RemoteDataSource {
+  Future<AuthenticationResponse> login(LoginRequest loginRequest);
+}
+
+class RemoteDataSourceImplementer implements RemoteDataSource {
+  final AppServiceClient _appServiceClient;
+
+  RemoteDataSourceImplementer(this._appServiceClient);
+
+  @override
+  Future<AuthenticationResponse> login(LoginRequest loginRequest) async {
+    return await _appServiceClient.login(
+      loginRequest.email,
+      loginRequest.password,
+      loginRequest.imei,
+      loginRequest.deviceType,
+    );
+  }
+}
