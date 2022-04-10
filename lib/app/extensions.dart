541cdebb43aa7da279934269d4b95907688c869b
hyuem <uhh0701@gmail.com>
2022-04-10 18:07:05 +0900

    Lecture 49 - Application Layer - Creating Extensions Functions

---
+++ lib/app/extensions.dart
@@ -0,0 +1,23 @@
+// extension on String
+
+extension NonNullString on String? {
+  String orEmpty() {
+    if (this == null) {
+      return "";
+    } else {
+      return this!;
+    }
+  }
+}
+
+// extensio on Integer
+
+extension NonNullInteger on int? {
+  int orZero() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
