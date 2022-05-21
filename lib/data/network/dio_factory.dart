f9224839333f036ad49f921f5bae55ef9bad1763
hyuem <uhh0701@gmail.com>
2022-05-21 15:45:07 +0900

    Lecture 63 - Data Layer - Adding Dio Logger interceptor

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,5 +1,7 @@
 import 'package:complete_advanced_flutter/app/constant.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 const String APPLICATION_JSON = "application/json";
 const String CONTENT_TYPE = "content-type";
@@ -25,6 +27,16 @@ class DioFactory {
       receiveTimeout: _timeOut,
       headers: headers,
     );
+
+    if (kReleaseMode) {
+      print("release mode no logs");
+    } else {
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+      ));
+    }
 
     return dio;
   }
